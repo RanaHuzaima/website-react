3132d1a2ff1c98032a761eff5d75d7b9b412d7a0
Rana Huzaima <huzaimadigital@gmail.com>
2023-11-04 16:09:56 +0500

    Add Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Components/Header/Header";
+import Navbar from "./Components/Navbar/Navbar";
 
 const App = () => {
   return (
     <>
       <Header />
+      <Navbar />
     </>
   );
 };
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="d-flex align-items-center justify-content-center py-3 px-5 gap-5 fs-4">
+        <a href="#" class="text-color text-decoration-none fw-medium">
+          HOME
+        </a>
+        <a href="#" class="text-color text-decoration-none fw-medium">
+          ABOUT US
+        </a>
+        <a href="#" class="text-color text-decoration-none fw-medium">
+          SERVICE
+        </a>
+        <a href="#" class="text-color text-decoration-none fw-medium">
+          PRICING
+        </a>
+        <a href="#" class="text-color text-decoration-none fw-medium">
+          BLOG
+        </a>
+        <a href="#" class="text-color text-decoration-none fw-medium">
+          CONTACT
+        </a>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
