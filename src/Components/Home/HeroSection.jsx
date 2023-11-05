46d3b59f5fcd65bbc8c11438ff10783688529aec
Rana Huzaima <huzaimadigital@gmail.com>
2023-11-05 13:21:57 +0500

    Fix Some Hero-Section Issues

--- src/Components/Home/HeroSection.jsx
+++ src/Components/Home/HeroSection.jsx
@@ -3,17 +3,20 @@ import React from "react";
 const HeroSection = () => {
   return (
     <>
-      <div class="herosection px-5 py-5 " >
+      <div class="herosection px-5 py-5 mw-100 ">
         <div class="py-5">
-          <h1 class="fw-bold mb-0">AMERICAN FOOD</h1>
-          <p class="fs-2 fw-medium">The Best Food Taste</p>
-          <p class="mb-5">
+          <h1 class="fw-bold mb-0 display-2">AMERICAN FOOD</h1>
+          <p class="display-4 fw-normal">The Best Taste Food</p>
+          <div class="pp mb-4"></div>
+          <p class="mb-5 fs-4">
             Lorem Ipsum available, but the majority have <br /> suffered
             alteration in some form, by injected <br /> humour, or randomised
             words
           </p>
-          <button class="p-3 btn border text-white ">ORDER NOW</button>{" "}
-          <i class="bi bi-arrow-left btn fs-3 border text-white"></i>
+          <div class="d-flex gap-1">
+            <button class="p-3 text-white btn-radius ">ORDER NOW</button>
+            <i class="bi bi-arrow-left fs-3 text-white btn-icon d-flex align-items-center justify-content-center"></i>
+          </div>
         </div>
       </div>
     </>
