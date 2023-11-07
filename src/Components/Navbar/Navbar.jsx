2d997c5924c65bd9181de7bf2cc96ccba32c0ad5
Rana Huzaima <huzaimadigital@gmail.com>
2023-11-07 15:20:15 +0500

    Make Navbar Responsive

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,26 +1,27 @@
 import React from "react";
 import "../../App.css";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <>
-      <div className="d-flex align-items-center justify-content-center py-3 px-5 gap-4 fs-4 navbar">
-        <a href="#" class="text-color text-decoration-none fw-medium">
+      <div className="text-center gap-4 py-3 px-5 fs-3 gap-4 d-none d-sm-block navbar">
+        <a href="#" class="text-color text-decoration-none fw-medium me-3">
           HOME
         </a>
-        <a href="#" class="text-color text-decoration-none fw-medium">
+        <a href="#" class="text-color text-decoration-none fw-medium me-3 ">
           ABOUT US
         </a>
-        <a href="#" class="text-color text-decoration-none fw-medium">
+        <a href="#" class="text-color text-decoration-none fw-medium me-3 ">
           SERVICE
         </a>
-        <a href="#" class="text-color text-decoration-none fw-medium">
+        <a href="#" class="text-color text-decoration-none fw-medium me-3 ">
           PRICING
         </a>
-        <a href="#" class="text-color text-decoration-none fw-medium">
+        <a href="#" class="text-color text-decoration-none fw-medium me-3 ">
           BLOG
         </a>
-        <a href="#" class="text-color text-decoration-none fw-medium">
+        <a href="#" class="text-color text-decoration-none fw-medium ">
           CONTACT
         </a>
       </div>
