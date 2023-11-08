1056b4eeddd221d576c9c6317bd11d8da6247f14
Rana Huzaima <huzaimadigital@gmail.com>
2023-11-08 13:34:58 +0500

    Make Video-Section responsive

--- src/Components/Video-Section/VideoSection.jsx
+++ src/Components/Video-Section/VideoSection.jsx
@@ -2,9 +2,44 @@ import React from "react";
 import "./VideoSection.css";
 const Video_Section = () => {
   return (
+    // <>
+    //   <div class="container-fluid text-color p-0 p-md-5 d-flex gap-5 main">
+    //     <div class="">
+    //       <p class="display-3 fw-bold">
+    //         Let food be the medicine medicine by thy food.
+    //       </p>
+    //       <p class="fs-4">
+    //         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliiss
+    //         fafkshd impedit atq Alias impedit atque et corrupti optio
+    //         consequatur ullam impedit atq Alias impedit atque et corrupti optio
+    //         consequatur ullam impedit atq Aliiss fafkshd dolores quos nobis?
+    //         Maxime.
+    //       </p>
+    //       <p class="fs-4">
+    //         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliiss
+    //         fafkshd impedit atq Alias impedit atque et corrupti optio.
+    //       </p>
+    //       <div class="d-flex gap-1">
+    //         <button class="p-3 text-white btn-radius ">READ MORE</button>
+    //         <i class="bi bi-arrow-left fs-3 text-white btn-icon d-flex align-items-center justify-content-center"></i>
+    //       </div>
+    //     </div>
+    //     <div class="position-relative mt-4">
+    //       <div class="video-border"></div>
+    //       <div class="color d-flex justify-content-end"></div>
+    //       <iframe
+    //         src="https://www.youtube-nocookie.com/embed/kMnaNYZTIS4?si=I1za6VrkrcZ9nQhw&amp;controls=0"
+    //         title="YouTube video player"
+    //         frameborder="0"
+    //         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    //         allowfullscreen
+    //       ></iframe>
+    //     </div>
+    //   </div>
+    // </>
     <>
-      <div class="container-fluid text-color p-0 p-md-5 d-flex gap-5 main">
-        <div class="">
+      <div class="container-fluid d-flex gap-video text-color p-0 p-md-5 main">
+        <div class="inner">
           <p class="display-3 fw-bold">
             Let food be the medicine medicine by thy food.
           </p>
@@ -15,7 +50,7 @@ const Video_Section = () => {
             consequatur ullam impedit atq Aliiss fafkshd dolores quos nobis?
             Maxime.
           </p>
-          <p class="fs-5">
+          <p class="fs-4">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliiss
             fafkshd impedit atq Alias impedit atque et corrupti optio.
           </p>
@@ -36,43 +71,6 @@ const Video_Section = () => {
           ></iframe>
         </div>
       </div>
-      {/* <div class="d-flex align-items-center justify-content-center gap-video text-color flex-wrap ">
-        <div>
-          <p class="display-3 fw-medium">
-            Let food be thy medicine  medicine by thy food.
-          </p>
-          <p class="fs-5 ">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliiss
-            fafkshd impedit atq
-             Alias impedit atque et corrupti optio consequatur ullam
-            impedit atq Aliiss fafkshd  Alias impedit atque et corrupti
-            optio consequatur ullam impedit atq Aliiss fafkshd
-             dolores quos nobis? Maxime.
-          </p>
-          <p class="fs-5 text-color ">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliiss
-            fafkshd impedit atq
-             Alias impedit atque et corrupti optio.
-          </p>
-          <div class="d-flex gap-1">
-            <button class="p-3 text-white btn-radius ">READ MORE</button>
-            <i class="bi bi-arrow-left fs-3 text-white btn-icon d-flex align-items-center justify-content-center"></i>
-          </div>
-        </div>
-        <div>
-          <div class="line2"></div>
-          <div class="color"></div>
-          <iframe
-            width="520"
-            height="390"
-            src="https://www.youtube-nocookie.com/embed/kMnaNYZTIS4?si=I1za6VrkrcZ9nQhw&amp;controls=0"
-            title="YouTube video player"
-            frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowfullscreen
-          ></iframe>
-        </div>
-      </div> */}
     </>
   );
 };
--- src/Components/Video-Section/VideoSection2.jsx
+++ src/Components/Video-Section/VideoSection2.jsx
@@ -1,15 +1,14 @@
 import React from "react";
+import "./VideoSection.css";
 
 const VideoSection2 = () => {
   return (
     <>
-      <div class="d-flex align-items-center justify-content-center gap-video text-color mt-5 flex-wrap ">
-        <div>
-          <div class="line2"></div>
-          <div class="color"></div>
+      <div class="container-fluid d-flex gap-video text-color p-0 p-md-5 mt-3 main">
+        <div class="position-relative">
+          <div class="video-border"></div>
+          <div class="color d-flex justify-content-end"></div>
           <iframe
-            width="520"
-            height="390"
             src="https://www.youtube-nocookie.com/embed/kMnaNYZTIS4?si=I1za6VrkrcZ9nQhw&amp;controls=0"
             title="YouTube video player"
             frameborder="0"
@@ -17,22 +16,20 @@ const VideoSection2 = () => {
             allowfullscreen
           ></iframe>
         </div>
-        <div>
-          <p class="display-3 fw-medium">
-            Let food be thy medicine <br /> medicine by thy food.
+        <div class="mt-4">
+          <p class="display-3 fw-bold">
+            Let food be the medicine medicine by thy food.
           </p>
-          <p class="fs-5 ">
+          <p class="fs-4">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliiss
-            fafkshd impedit atq
-            <br /> Alias impedit atque et corrupti optio consequatur ullam
-            impedit atq Aliiss fafkshd <br /> Alias impedit atque et corrupti
-            optio consequatur ullam impedit atq Aliiss fafkshd
-            <br /> dolores quos nobis? Maxime.
+            fafkshd impedit atq Alias impedit atque et corrupti optio
+            consequatur ullam impedit atq Alias impedit atque et corrupti optio
+            consequatur ullam impedit atq Aliiss fafkshd dolores quos nobis?
+            Maxime.
           </p>
-          <p class="fs-5 text-color ">
+          <p class="fs-4">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliiss
-            fafkshd impedit atq
-            <br /> Alias impedit atque et corrupti optio.
+            fafkshd impedit atq Alias impedit atque et corrupti optio.
           </p>
           <div class="d-flex gap-1">
             <button class="p-3 text-white btn-radius ">READ MORE</button>
