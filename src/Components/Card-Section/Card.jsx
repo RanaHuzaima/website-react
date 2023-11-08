171c3274f865882d1573167e43bf5605fd6d3371
Rana Huzaima <huzaimadigital@gmail.com>
2023-11-08 17:07:03 +0500

    Make Card-section responsive

--- src/Components/Card-Section/Card.jsx
+++ src/Components/Card-Section/Card.jsx
@@ -3,46 +3,52 @@ import React from "react";
 const Card = () => {
   return (
     <>
-      <div class="main-card d-flex flex-row justify-content-around flex-wrap align-items-center">
-        <div class="d-flex flex-column">
-          <div class="card-title text-center text-white p-4 fs-1">
-            <p class="fs-1 lh-1">Basic</p>
-            <p class="fw-bold lh-1">$50</p>
+      <div className="container-fluid px-5 main-card justify-content-md-between d-flex flex-row flex-wrap">
+        <div className="d-flex flex-column  mb-3">
+          <div className="card-title text-center text-white p-4 fs-1">
+            <p className="fs-1 lh-1">Basic</p>
+            <p className="fw-bold lh-1">$50</p>
           </div>
-          <div class="card-body text-center text-color p-5 fs-5">
+          <div className="card-body text-center text-color p-5 fs-5">
             <p>6 Pics Chiken</p>
             <p>6 Pics Chiken</p>
             <p>6 Pics Chiken</p>
             <div>
-              <button class="p-3 text-white btn-radius mt-2">BUY NOW</button>
+              <button className="p-3 text-white btn-radius mt-2">
+                BUY NOW
+              </button>
             </div>
           </div>
         </div>
-        <div class="d-flex flex-column">
-          <div class="card-title text-center text-white p-4 fs-1">
-            <p class="fs-1 lh-1">Basic</p>
-            <p class="fw-bold lh-1">$50</p>
+        <div className="d-flex flex-column mb-3">
+          <div className="card-title text-center text-white p-4 fs-1">
+            <p className="fs-1 lh-1">Basic</p>
+            <p className="fw-bold lh-1">$50</p>
           </div>
-          <div class="card-body text-center text-color p-5 fs-5">
+          <div className="card-body text-center text-color p-5 fs-5">
             <p>6 Pics Chiken</p>
             <p>6 Pics Chiken</p>
             <p>6 Pics Chiken</p>
             <div>
-              <button class="p-3 text-white btn-radius mt-2">BUY NOW</button>
+              <button className="p-3 text-white btn-radius mt-2">
+                BUY NOW
+              </button>
             </div>
           </div>
         </div>
-        <div class="d-flex flex-column">
-          <div class="card-title text-center text-white p-4 fs-1">
-            <p class="fs-1 lh-1">Basic</p>
-            <p class="fw-bold lh-1">$50</p>
+        <div className="d-flex flex-column mb-3">
+          <div className="card-title text-center text-white p-4 fs-1">
+            <p className="fs-1 lh-1">Basic</p>
+            <p className="fw-bold lh-1">$50</p>
           </div>
-          <div class="card-body text-center text-color p-5 fs-5">
+          <div className="card-body text-center text-color p-5 fs-5">
             <p>6 Pics Chiken</p>
             <p>6 Pics Chiken</p>
             <p>6 Pics Chiken</p>
             <div>
-              <button class="p-3 text-white btn-radius mt-2">BUY NOW</button>
+              <button className="p-3 text-white btn-radius mt-2">
+                BUY NOW
+              </button>
             </div>
           </div>
         </div>
